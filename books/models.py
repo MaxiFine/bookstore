8f9d1641cf4126a94316591a26ff15a5114ecac7
MaxFine <gibboel5@gmail.com>
2023-01-26 11:27:34 +0000

    setup for pillow, reviews, done

--- books/models.py
+++ books/models.py
@@ -1,16 +1,41 @@
+import uuid
+from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.db import models
 
+
 # Create your models here.
 class Book(models.Model):
+    # the error started # when i added this line from the book
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, 
+    editable=False)   
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    cover = models.ImageField(upload_to='covers/', blank=True) # new
 
 
     def __str__(self):
         return self.title
 
+
     def get_absolute_url(self): # new
+        return reverse('book_detail',  args=[str(self.id)])
+
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, 
+            related_name='reviews')
+    review = models.CharField(max_length=255)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.review
+
+    
+    def get_absolute_url(self):
         return reverse('book_detail', args=[str(self.id)])
+
+
 
--- config/settings.py
+++ config/settings.py
@@ -142,6 +142,9 @@ STATICFILES_FINDERS = [ # new
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
+MEDIA_URL = '/media/' 
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
